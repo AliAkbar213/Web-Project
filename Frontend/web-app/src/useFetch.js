48de76392868eee56056f5c2d6c28c371c05502c
AliAkbar213 <aliakbarbhura@gmail.com>
2026-06-04 23:33:12 +0300

    added json server to test REST api. must be removed later.

--- Frontend/web-app/src/useFetch.js
+++ Frontend/web-app/src/useFetch.js
@@ -2,27 +2,26 @@ import { useState, useEffect } from "react";
 
 function useFetch(){
 
-    const [data , setData] = useState([{}]);
+    const [data , setData] = useState([]);
     const [loading , setLoading] = useState(true);
     const [error , setError] = useState(null);
 
     useEffect(() => {
-        fetch("/data").then(
+        fetch("http://localhost:3001/products").then(
             (res) => {
                 if (!res.ok){
                     setError("could not fetch data")
                 }
                 return res.json();
-            }
-        ).then(
+            })
+            .then(
             (data) => {
-                setData(data)
+                setData(data);
                 setLoading(false);
-            }
-        ).catch(err => {
-            setError(err)
-            }                
-        )
+            })
+            .catch(err => {
+                setError(err);
+            })
     },[])
 
     return {data, loading, error}
